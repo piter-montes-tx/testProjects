941cacbf071773f950b362a8ad0cb3660b5a8e4c
piter montes <piter.montes.tx@gmail.com>
2016-04-28 12:51:37 -0400

    updating change language feature

--- features/step_definitions/changeLanguage_steps.rb
+++ features/step_definitions/changeLanguage_steps.rb
@@ -22,12 +22,22 @@ And(/^I tap on "([^"]*)" option from the "([^"]*)" List$/) do |option, listView|
     else
       puts("No option defined for #{listView}")
   end
-
 end
 
+And(/^I tap on "([^"]*)" option from the "([^"]*)" lists$/) do |option, listView|
+  case listView
+    when 'Quick Menu' then
+      # xpath("//android.widget.ListView//android.widget.TextView[@text=\"#{option}\"]").click
+      xpath("//android.widget.FrameLayout[@index='0']/child::android.widget.ListView/child::android.widget.LinearLayout/child::android.widget.RelativeLayout/child::android.widget.TextView").click
+      sleep 1
+    else
+      puts("No option defined for #{listView}")
+  end
+end
 When(/^I tap on "([^"]*)" item option from the "([^"]*)" list$/) do |item, listView|
   case listView
     when 'Language' then
+      scroll_to ("#{item}")
       xpath("//android.widget.ListView//android.widget.CheckedTextView[@text=\"#{item}\"]").click
       sleep 1
     else
@@ -42,17 +52,13 @@ Then(/^I should see the application language changed to "([^"]*)" language title
   sleep 1
 end
 
-When(/^I select "([^"]*)" from "([^"]*)" menu$/) do |arg1, arg2|
-  pending
-end
+# When(/^I select "([^"]*)" from "([^"]*)" menu$/) do |arg1, arg2|
+#   pending
+# end
 
-Then(/^I should see "([^"]*)" item option is displayed on "([^"]*)" screen$/) do |arg1, arg2|
-  pending
-end
-
-When(/^I tap on "([^"]*)" option from the "([^"]*)" screen$/) do |arg1, arg2|
-  pending
-end
+# Then(/^I should see "([^"]*)" item option is displayed on "([^"]*)" screen$/) do |arg1, arg2|
+#   pending
+# end
 
 Then(/^I should see "([^"]*)" item option is displayed on the "([^"]*)" list$/) do |item, listView|
   case listView
@@ -70,3 +76,20 @@ Then(/^I should see "([^"]*)" item option is displayed on "([^"]*)" list$/) do |
   end
 
 end
+
+
+# Then /^I scroll (left|right|up|down) until I see "([^"]*)" limit (\d+)$/ do |dir, name, limit|
+#   # unless exists?(name)
+#   #   count = 0
+#   #   begin
+#   #     scroll("scrollView index:0", dir)
+#   #     step_pause
+#   #     count = count + 1
+#   #   end while ((not exists?(name)) and count < limit.to_i)
+#   # end
+#   # unless exists?(name)
+#   #   screenshot_and_raise "i scrolled '#{dir}' '#{limit}' times but did not see #{name}"
+#   # end
+#
+#   scroll_to name
+# end
