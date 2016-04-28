bebf8c46304177d338bf97580c9f590cbff9e9bb
Miguel A. Coca <miguel.coca@truextend.com>
2016-04-28 16:09:30 -0400

    adding couple scenarios and its definitions

--- features/step_definitions/createAccount.rb
+++ features/step_definitions/createAccount.rb
@@ -16,8 +16,9 @@ When(/^I fill in email Address field with "(.*)"$/) do |arg|
   id('user_email').type "#{arg}"
 end
 
-And(/^I finn in Password field with "([^"]*)"$/) do |arg|
-  id('user_password').type "#{arg}"
+And(/^I fill in Password field with "(.*)"$/) do |arg|
+  puts "Password: #{arg}"
+  id('user_password').send_keys "#{arg}"
 end
 
 And(/^I click "([^"]*)" button$/) do |arg|
@@ -32,4 +33,33 @@ end
 
 And(/^I should be able to press "(.*)" button$/) do |arg|
   button("#{arg}").click
+end
+
+#And(/^I fill in Password field with "(.*)"$/) do |arg|
+#  step "I fill in Password field with #{arg}"
+#end
+
+When(/^I Click Sign in button$/) do
+  id('sign_in_button').click
+end
+
+Then(/^I should be logged to the Search Activity$/) do
+  #wait{text('Search')}
+  txt = id('search_title').text
+  expect(txt).to eq("Search")
+end
+
+And(/^I should see "(.*)" email account$/) do |arg|
+  step "I click on the hamburguer button"
+  txt = id('navigation_drawer_header_user_name').text
+
+  expect(txt).to eq("#{arg}")
+end
+
+Then(/^I should see error message popup$/) do
+  exists{id('title')}?(expect(id('title').text).to eq('Oops! Sign in failed')):(raise 'expected message popup not found.')
+end
+
+And(/^I should click "([^"]*)" button$/) do |arg|
+  button('OK').click
 end
